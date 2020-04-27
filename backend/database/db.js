3ae1a9959f403dd07a928122c8c9e73065da5021
Matthew Stitt <mstitt@us.ibm.com>
2020-04-27 11:42:54 -0400

    new server.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,16 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+const config = require('../../config.json');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mstitt:1sabrina@cluster0-kzh09.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  MongoClient.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+  MongoClient.Promise = global.Promise;
+  // perform actions on the collection object
+  client.close();
+});
 
-module.exports = {
-    User: require('../users/user.model')
-};
+// module.exports = {
+//     User: require('../src/app/_model/user.model')
+// };
+
